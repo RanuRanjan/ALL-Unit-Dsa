1c230efd4b75cc0d000e04a0d150ecab31a3c284
RanuRanjan <ranuranjan25@gmail.com>
2022-02-12 14:38:58 +0530

    new dsa

--- test.js
+++ test.js
@@ -1,16 +1,36 @@
-function compare(a,b){
-    if(a>b){
-        return "True"
-    }
+// // function compare(a,b){
+// //     if(a>b){
+// //         return "True"
+// //     }
     
-    else if(a===b){
-        return "False"
-    }
-    else{
-        return "False"
-    }
-}
-console.log(compare("112","111"));
-console.log(compare("525","1111"));
-console.log(compare("11","0"));
-console.log(compare("1",'1'));
+// //     else if(a===b){
+// //         return "False"
+// //     }
+// //     else{
+// //         return "False"
+// //     }
+// // }
+// // console.log(compare("112","111"));
+// // console.log(compare("525","1111"));
+// // console.log(compare("11","0"));
+// // console.log(compare("1",'1'));
+
+
+// var arr=[1,2,2,3,5]
+
+// console.log(arr.length);
+
+// delete arr[1]
+// console.log(arr);
+// arr.pop()
+// console.log(arr[1]);
+// console.log(typeof arr[1]);
+// console.log(arr.length);
+// log
+
+console.log(1<2<3);
+console.log(3>2>1);
+
+css  interview center a div
+
+how mays we forms in react
