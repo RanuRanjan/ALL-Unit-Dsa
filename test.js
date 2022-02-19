6ea2899d5c931cf1cfaafbde6c5997032ab27467
RanuRanjan <ranuranjan25@gmail.com>
2022-02-19 17:39:54 +0530

    update

--- test.js
+++ test.js
@@ -1,60 +1,12 @@
-// // function compare(a,b){
-// //     if(a>b){
-// //         return "True"
-// //     }
-    
-// //     else if(a===b){
-// //         return "False"
-// //     }
-// //     else{
-// //         return "False"
-// //     }
-// // }
-// // console.log(compare("112","111"));
-// // console.log(compare("525","1111"));
-// // console.log(compare("11","0"));
-// // console.log(compare("1",'1'));
-
-
-// var arr=[1,2,2,3,5]
-
-// console.log(arr.length);
-
-// delete arr[1]
-// console.log(arr);
-// arr.pop()
-// console.log(arr[1]);
-// console.log(typeof arr[1]);
-// console.log(arr.length);
-// log
-
-console.log(1<3);
-console.log(3>2);
-console.log(true>1);
-
-console.log(0==true);
-
-
-
-let obj={
-    x:2,
-    y:3
+async function api(){
+    return new Promise (resolve =>{
+        setTimeout (()=>{resolve ("b")},50)
+    })
 }
 
-console.log(Object.keys(obj));
-console.log(Object.values(obj));
-// console.log(Object(obj));
-
-
-
-
-
-
-
-
-
-
-// (3>2>1) true 1
-// css  interview center a div
-
-// how mays we forms in react
+async function log(){
+    setTimeout (()=>console.log("a"),10)
+    console.log(await api());
+    console.log('c');
+}
+log()
